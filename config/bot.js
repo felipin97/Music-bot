5a0bf66f7f9874e18bcadbb1d6f117a2ab514e07
felipin97 <84291030+felipin97@users.noreply.github.com>
2021-05-17 00:45:41 -0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODQzNTAyNTg2NTA1MTk5NjQ2.YKEzAA.4fjutNCDTu7Htu7c6qpoPn3wgO8',
+        prefix: '!',
+        activity: 'ONLINE',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
